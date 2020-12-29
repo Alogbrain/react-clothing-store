67b2a56ad633c01356dedec73ef3f77e67a5ed8d
annguyen <nnn.annguyen@elca.ch>
2020-12-29 17:36:10 +0700

    coding fetch data from back end

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,18 +1,47 @@
 import {all, takeLatest, call, put} from 'redux-saga/effects'
 import ShopActionTypes from "./shop.types";
+import axios from "axios";
 
 import {firestore, convertCollectionsSnapshotToMap} from "../../firebase/firebase.utils";
 import {fetchCollectionsSuccess, fetchCollectionsFailure} from "./shop.actions";
 
+export const fetchCollections = () =>{
+        return axios.get(`/collection/getAll`).then(
+            res => {
+                return res.data
+            }).catch(
+                err =>{
+                    console.log("ERROR" + err)
+                    throw err;
+                }
+        )
+}
+
 export function* fetchCollectionsAsync() {
     try {
-        const collectionRef = firestore.collection("collections");
-        const snapshot = yield collectionRef.get();
-        const collectionsMap = yield call(
-            convertCollectionsSnapshotToMap,
-            snapshot
-        );
-        yield put(fetchCollectionsSuccess(collectionsMap));
+        // const collectionRef = firestore.collection("collections");
+        // const snapshot = yield collectionRef.get();
+
+        // axios.get(`/collection/getAll`).then(
+        //     res => {
+        //         console.log(res)
+        //         const collectionsMap = call(
+        //             convertCollectionsSnapshotToMap,
+        //             res.data
+        //         );
+        //         console.log(collectionsMap)
+        //         put(fetchCollectionsSuccess(collectionsMap));
+        //     }
+        // )
+
+        const snapshot = fetchCollections();
+
+
+        // const collectionsMap = yield call(
+        //     convertCollectionsSnapshotToMap,
+        //     snapshot
+        // );
+        // yield put(fetchCollectionsSuccess(collectionsMap));
     } catch (error) {
         yield put(fetchCollectionsFailure(error.message));
     }
